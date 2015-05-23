c0a95f541a78062ace5e3f441c13acc88a2789f4
donnapep <donnapep@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-23 18:18:31 +0000

    Release 2.1.12
    git-svn-id: https://plugins.svn.wordpress.org/book-review/tags/2.1.12@1166392 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/class-book-review-deactivator.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://donnapeplinskie.com
+ * @since      2.1.8
+ *
+ * @package    Book_Review
+ * @subpackage Book_Review/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      2.1.8
+ * @package    Book_Review
+ * @subpackage Book_Review/includes
+ * @author     Donna Peplinskie <donnapep@gmail.com>
+ */
+class Book_Review_Deactivator {
+  /**
+   *
+   * @since    2.1.8
+   */
+  public static function deactivate() {
+
+  }
+}
